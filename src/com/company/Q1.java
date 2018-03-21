494d53fd1da1cfde5c677474a924bdc9713b2de1
1700022775 <pengsen1994@qq.com>
2018-03-21 16:55:33 +0800

    修改Q1

--- src/com/company/Q1.java
+++ src/com/company/Q1.java
@@ -1,8 +1,11 @@
 package com.company;
 
+import java.net.SocketPermission;
+
 public class Q1 {
     public static void main(String[] args) {
         System.out.println("Welcome to Beijing.");
+        System.out.println("My name is pengsen.");
     }
 
 }
